b202fabab8c1b63fa6bf43f481c58ef6b4733990
murata.kazuma <murata.kazuma@Donuts-N561.local>
2017-04-26 19:47:05 +0900

    絞り込み条件追加

--- twitter_analysis_func.py
+++ twitter_analysis_func.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from time import sleep
+from datetime import datetime as dt
 import collections
 import twitter_generic_func as tg
 import const as C
@@ -14,10 +15,27 @@ class Friend:
         self.bio = bio
 
 
+class User:
+    def __init__(self, id, name, description, friends_count, created_at):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.friends_count = friends_count
+        self.created_at = created_at
+
+
+def print_step_log(step_name, index, list_len):
+    print step_name + " step:" + str(index+1) + "/" + str(list_len)
+
+
+def print_query_error(action_name, usr_id):
+    print "Exception(" + action_name + ") USER_ID:" + str(user_id)
+
+
 # 例: 自分のフォロワーのA,B,Cについて分析する時
 # A, B, Cがそれぞれフォローしている人は、
 # A, B, Cのうち何人にフォローされているのかを分析する。
-# Friendオブジェクト(ID, 名前, 人数)の配列を返す。
+# Friendオブジェクト(ID, 名前, 人数, BIO)の配列を返す。
 def analysys_follower_friends():
 
     # 上限の5000人分取得
@@ -34,7 +52,7 @@ def analysys_follower_friends():
             ids = tg.get_friend_ids(f_id, 5000) # 5000件取得できるIDの方を使う
             friend_ids.extend(ids)
         except:
-            print "No Responce(USER_ID: " + str(f_id) + ")"
+            print_query_error("get_friend_ids", f_id)
         sleep(60)
 
 
@@ -53,6 +71,76 @@ def analysys_follower_friends():
         friend = Friend(id=key, name=prof['name'], count=value, bio=prof['description'])
         friends.append(friend)
         sleep(1.5)
+
+    # フォローされている数の昇順に並び替え
+    friends = sorted(friends, key=lambda u: u.count, reverse=True)
+    return friends
+
+
+# フォロワーの中で2016年以前の登録ユーザをフォロー数の降順に並べて
+# 分析したアカウントをFriendインスタンスにしてリストで返す
+def test():
+    # 上限の5000人分取得
+    follower_ids = tg.get_follower_ids(C.ANALYSYS_USER_ID, 5000)
+
+    followers = []
+
+    # Userクラスとしてリストを作っておくと絞り込みが楽そうだった
+    for index, follower_id in enumerate(follower_ids):
+        print_step_log("CreateFollowersList", index, len(follower_ids))
+        try:
+            prof = tg.get_user_profile(follower_id)
+            user = User(id=prof['id'],
+                        name=prof['name'],
+                        description=prof['description'],
+                        friends_count=prof['friends_count'],
+                        created_at=dt.strptime(prof['created_at'], "%a %b %d %H:%M:%S +0000 %Y"))
+            followers.append(user)
+            sleep(1)
+        except:
+            print_query_error("get_user_profile", follower_id)
+
+
+    # 2016年以前のユーザで絞り込みしフォロー数の多い順で並べる
+    followers = filter(lambda obj:obj.created_at.year < 2016, followers)
+    followers = sorted(followers, key=lambda obj: obj.friends_count, reverse=True)
+
+    # とりあえず150人に絞る
+    followers = followers[0:150]
+
+    # フォロワーがフォローしている人
+    friend_ids = []
+
+    # 1回クエリを飛ばすごとに1分休む
+    for index, follower in enumerate(followers):
+        print_step_log("CreateFriendIDs", index, len(followers))
+        try:
+            ids = tg.get_friend_ids(follower.id, 5000) # 5000件取得できるIDの方を使う
+            friend_ids.extend(ids)
+        except:
+            print_query_error("get_friend_ids", follower.id)
+        sleep(60)
+
+    # フレンドのIDをキーにして、フレンドがフォロワーにフォローされている人数を格納
+    friends_counter_dict = collections.Counter(friend_ids)
+
+    # フレンドのクラスの配列を作る
+    # もっと良い書き方がありそう
+    friends = []
+    step = 0
+    for key, value in sorted(friends_counter_dict.items(), key=lambda x: x[1], reverse=True):
+        step += 1
+        print_step_log("CreateFriendList", step, len(friends_counter_dict))
+
+        # とりあえず5人以上にフォローされているアカウントを取る
+        if value > 5:
+            try:
+                prof = tg.get_user_profile(key)
+                friend = Friend(id=key, name=prof['name'], count=value, bio=prof['description'])
+                friends.append(friend)
+            except:
+                print_query_error("get_user_profile", key)
+            sleep(1)
 
     # フォローされている数の昇順に並び替え
     friends = sorted(friends, key=lambda u: u.count, reverse=True)
--- twitter_test.py
+++ twitter_test.py
@@ -5,8 +5,8 @@ import csv
 import twitter_analysis_func as ta
 import const
 
-# 実行用
-def main():
+
+def run_analysys_base():
     friends = ta.analysys_follower_friends()
 
     # ログだと見辛いのでとりあえず今はCSVに書き出す
@@ -19,6 +19,26 @@ def main():
             print str(friend.id) + " " + friend.name
             row = [str(friend.id), unicode(friend.name).encode("utf-8"), str(friend.count), unicode(friend.bio).encode("utf-8")]
             writer.writerow(row)
+
+
+def run_analysys_ex1():
+    friends = ta.test()
+
+    # ログだと見辛いのでとりあえず今はCSVに書き出す
+    with open('follower_analytics.csv', 'w') as f:
+        writer = csv.writer(f, lineterminator='\n')
+        header = ["ID", "Name", "Count", "Bio"]
+        writer.writerow(header)
+
+        for friend in friends:
+            print str(friend.id) + " " + friend.name
+            row = [str(friend.id), unicode(friend.name).encode("utf-8"), str(friend.count), unicode(friend.bio).encode("utf-8")]
+            writer.writerow(row)
+
+
+# 実行用
+def main():
+    run_analysys_ex1()
 
 
 if __name__ == "__main__":
