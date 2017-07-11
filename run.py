d8e255a1801dbe0f8a7f85eb40829115e794f728
k-murata08 <murata.kazuma@donuts.ne.jp>
2017-07-11 16:47:40 +0900

    グラフ導入

--- run.py
+++ run.py
@@ -5,9 +5,12 @@ import csv
 import twitter_analysis_func as ta
 import time
 import sys
+import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
 
 
 def run_analysys_followee():
+    fp = FontProperties(fname='/Users/murata.kazuma/Library/Fonts/ipag.ttf');
     friends = ta.analysys_follower_friends()
 
     # ログだと見辛いのでとりあえず今はCSVに書き出す
@@ -16,7 +19,16 @@ def run_analysys_followee():
         header = ["ID", "Name", "Count", "FollowersCount", "Bio", "FollowRate[%]", "FollowerRatio", "Factor"]
         writer.writerow(header)
 
-        for friend in friends:
+        pltleft = []
+        pltheight = []
+        pltlabels = []
+
+        for i, friend in enumerate(friends):
+            if i < 10:
+                pltleft.append(i)
+                pltheight.append(friend.count)
+                pltlabels.append(friend.name)
+
             row = [friend.id,
                    unicode(friend.name).encode("utf-8"),
                    friend.count,
@@ -26,6 +38,16 @@ def run_analysys_followee():
                    friend.follow_ratio,
                    friend.factor]
             writer.writerow(row)
+
+        fig = plt.figure()
+        fig.subplots_adjust(bottom=0.2)
+        ax = fig.add_subplot(1, 1, 1)
+        ax.bar(pltleft, pltheight)
+        ax.set_xticks(pltleft)
+        ax.set_xticklabels(pltlabels, rotation=45)
+        for label in ax.get_xticklabels():
+            label.set_fontproperties(fp)
+        plt.savefig('followee_analitics.png')
 
 
 def run_analysys_morpheme():
@@ -50,6 +72,9 @@ def main():
         run_analysys_followee()
     elif sys.argv[1] == "morpheme":
         run_analysys_morpheme()
+    else:
+        print "Invalid Argument."
+        print "Collect is followee or morpheme."
 
     elapsed_time = time.time() - start_time
     print ("elapsed_time:{0}".format(int(elapsed_time))) + "[sec]\n"
