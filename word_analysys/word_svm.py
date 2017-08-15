cf5a36131a8898942b6362811f184ec1d585edff
k-murata08 <murata.kazuma@donuts.ne.jp>
2017-08-15 17:09:42 +0900

    RFのパラメータ追加

--- word_analysys/word_svm.py
+++ word_analysys/word_svm.py
@@ -35,48 +35,31 @@ def main():
 
     data_train, data_test, label_train, label_test = train_test_split(vecs, document_labels, test_size=0.3)
 
-    tuned_parameters = [{'n_estimators': [10, 30, 50, 70, 90, 110, 130, 150], 'max_features': ['auto', 'sqrt', 'log2', None]}]
+    tuned_parameters = [
+        {
+            'n_estimators': [5, 10, 30, 50, 70, 90, 130, 300],
+            'max_features': ['auto', 'sqrt', 'log2', None],
+            'n_jobs': [1],
+            'random_state': [0],
+            'min_samples_split': [3, 5, 10, 15, 20, 25, 30, 40, 50, 100],
+            'max_depth': [3, 5, 10, 15, 20, 25, 30, 40, 50, 100]
+        }
+    ]
 
-    clf = GridSearchCV(RandomForestClassifier(), tuned_parameters, cv=2, scoring='accuracy', n_jobs=-1)
+    clf = GridSearchCV(RandomForestClassifier(), tuned_parameters, cv=2, scoring='accuracy')
     clf.fit(data_train, label_train)
 
     print "ベストパラメタを表示"
     print clf.best_estimator_
+    print ""
 
     print("トレーニングデータでCVした時の平均スコア")
     for params, mean_score, all_scores in clf.grid_scores_:
         print "{:.3f} (+/- {:.3f}) for {}".format(mean_score, all_scores.std() / 2, params)
+    print ""
 
     y_true, y_pred = label_test, clf.predict(data_test)
     print classification_report(y_true, y_pred)
-
-    #dic = corpora.Dictionary(training_x)
-
-    # 単語辞書から出現頻度の少ない単語及び出現頻度の多すぎる単語を排除
-    #dic.filter_extremes(no_below=5, no_above=0.3)
-
-    # Bag of Wordsベクトルの作成
-
-    #bow_corpus = [dic.doc2bow(d) for d in documents]
-
-    # TF-IDFによる重み付け
-    #tfidf_model = models.TfidfModel(bow_corpus)
-    #tfidf_corpus = tfidf_model[bow_corpus]
-
-    # LSIによる次元削減
-    #lsi_model = models.LsiModel(tfidf_corpus, id2word=dic, num_topics=1000)
-    #lsi_corpus = lsi_model[tfidf_corpus]
-
-    #svc = svm.SVC()
-    #cs = [0.001, 0.01, 0.1, 1]
-    #gammas = [0.001, 0.01, 0.1, 1]
-    #parameters = {'kernel': ['rbf'], 'C': cs, 'gamma': gammas}
-    #clf = grid_search.GridSearchCV(svc, parameters)
-    #clf.fit(tfidf_corpus, training_y)
-
-
-    #score = clf.score(test_x, test_y)
-    #print ('Test score: ' + str(score))
 
 
 def parse_word_list(text):
